269a0ebc454f3b0bc5ece3d9815814b697741b74
Aaron Colbert <aaron-colbert@hotmail.com>
2018-07-21 12:55:16 -0400

    initial commit with DB and Seeds

---
+++ model/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devouered) VALUES ("chicken", false);
+INSERT INTO burgers (burger_name, devouered) VALUES ("Cheese Burger", true);
+INSERT INTO burgers (burger_name, devouered) VALUES ("Blue Cheese Burger", false);
